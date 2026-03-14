dd50f43d4f7674d8dbda87d8bff76cd4c8a093fd
Andcool-Systems <drolll2007@gmail.com>
2026-03-14 23:19:11 +0300

    fix: update layout metadata and icons for localization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,20 +5,23 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata: Metadata = {
-  title: 'Fishing Game',
-  description: 'PepeLand Fishing Mini-Game',
+    title: 'РЫБАЛКА · Повязки Pepeland',
+    description: 'Рыбалка и магазин макарон',
+    icons: {
+        icon: '/static/icons/icon.svg',
+        shortcut: '/static/icons/icon.svg',
+        apple: '/static/icons/icon.svg'
+    }
 };
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ru" className={inter.className}>
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="ru" className={inter.className}>
+            <body>{children}</body>
+        </html>
+    );
 }
